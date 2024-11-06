da2efa16f46130440b93c790ab82068d03c9909b
Alan <32477198+TaoHighEn@users.noreply.github.com>
2024-11-06 19:19:03 +0800

    Initial commit

---
+++ IoTWebPanel/IoTWebPanel/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using IoTWebPanel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IoTWebPanel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            using (var db = new ALNDBContext())
+            {
+                var ans = db.EventLogs.SingleOrDefault();
+                return View(ans);
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
